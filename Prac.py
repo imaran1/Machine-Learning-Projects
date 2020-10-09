d11f8c434d0149758c433ac5657dbca30161aa85
Imaran Ansari <31735208+imaran1@users.noreply.github.com>
2020-10-09 09:52:24 +0530

    Initial commit

---
+++ Prac.py
@@ -0,0 +1,38 @@
+# var1=1
+# var2=2
+# var3="3"
+# print(var1+var1+var3)
+
+# def cal(num1, num2=4):
+#     res=num1*num2
+#     print(res)
+# cal(5,7)
+#
+# x = 36 / 4 * (3 +  2) * 5 + 2
+#
+# print('x', x)
+
+# sampleList = ["Jon", "Kelly", "Jessa"]
+#
+# sampleList.append(2, "Scott")
+#
+# print('sampleList', sampleList)
+
+# var= "James Bond"
+#
+# print(var[2::-1])
+#
+# listOne = [20, 40, 60, 80]
+#
+# listTwo = [20, 40, 60, 80]
+#
+# print(listOne == listTwo)
+#
+# print(listOne is listTwo)
+
+# str = "pynative"
+# print (str[1:3])
+
+var = "Jerry" * 2  * 3
+
+print(var)
